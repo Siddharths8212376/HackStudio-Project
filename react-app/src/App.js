e1ab5cd5fe25428afa765c9f853a7470cc892594
nashvincent <nashvincent32@gmail.com>
2020-02-22 18:32:54 +0530

    Added 3 pages

---
+++ react-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+//import SignIn from './components/SignIn';
+//import SignUp from './components/SignUp'
+import Landing from './components/Landing'
+
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
