ee9cafd4b769338b80f3008307d18fe271c47bba
Siddharths8212376 <siddharth8212376@gmail.com>
2020-02-23 02:44:29 +0530

    updated the app.js file

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 //import SignIn from './components/SignIn';
 //import SignUp from './components/SignUp'
 
 import Landing from './components/Landing'
-
-function App() {
-  return (
-    <div className="App">
-      <Landing />
-    </div>
-  );
+// convert the stateless widget to stateful widget
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        <Landing />
+      </div>
+    );
+  }
 }
+
 
 export default App;
